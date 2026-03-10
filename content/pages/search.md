234085a7d293c21532c2f1b43874121f070a5172
ZheXun <limzhexun2000@gmail.com>
2026-03-10 22:29:37 +0800

    Initial skeleton commit

---
+++ content/pages/search.md
@@ -0,0 +1,4 @@
+Title: Search
+Date: 2026-03-10
+
+Search page coming soon.
---
+++ pelicanconf.py
@@ -0,0 +1,49 @@
+AUTHOR = 'Zhe Xun'
+SITENAME = 'Recipe Lab'
+SITEURL = ""
+THEME = "themes/recipes"
+PATH = "content"
+ARTICLE_PATHS = ['recipes']
+
+TIMEZONE = 'Asia/Singapore'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    # ("Pelican", "https://getpelican.com/"),
+    # ("Python.org", "https://www.python.org/"),
+    # ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    # ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    # ("You can add links in your config file", "#"),
+    # ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+import json
+
+# Custom Jinja filter
+def parse_json(value):
+    try:
+        return json.loads(value)
+    except:
+        return []
+
+# Register it with Jinja
+JINJA_FILTERS = {
+    'parse_json': parse_json
+}
